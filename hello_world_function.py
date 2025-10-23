f5a281c3f12631651204206c1b58d7392f3cddb7
Augustine Khumalo <mr.a.s.khumalo@gmail.com>
2025-10-23 16:12:55 +0200

    introducing the concept of functions, I added a function and called it inside main to print Hello World!

---
+++ hello_world_function.py
@@ -0,0 +1,11 @@
+"""
+Date: 2025-10-23
+Author: Augustine Khumalo   
+Description: A simple Python script that prints "Hello, World!" to the console from a function.
+"""
+
+def hello_world():
+    print("Hello, World!")
+
+if __name__ == "__main__":
+    hello_world()
