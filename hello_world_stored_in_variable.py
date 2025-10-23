3c10bcf7167a6457af516fe2a76f6e1869a3dab0
Augustine Khumalo <mr.a.s.khumalo@gmail.com>
2025-10-23 16:28:54 +0200

    adding a varaible that stores a message, to be printed

---
+++ hello_world_stored_in_variable.py
@@ -0,0 +1,9 @@
+"""
+Date: 2025-10-23
+Author: Augustine Khumalo
+Description: A simple Python script that prints "Hello, World!" to the console, with the message stored in a variable.
+"""
+
+message = "Hello, World!"
+
+print(message)
