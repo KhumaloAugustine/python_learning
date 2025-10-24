a8629f5a7891dc5c5fc3d5c6fad932720bd31525
Augustine Khumalo <mr.a.s.khumalo@gmail.com>
2025-10-24 15:51:16 +0200

    adiding additional_operator file that create a variable and assign the the result of addition then print the result

---
+++ addion_operator.py
@@ -0,0 +1,9 @@
+"""
+Date: 2025-10-24
+Author: Augustine Khumalo
+Description: A simple Python script that defines a variable with the addition operator and prints the result.
+"""
+#Creating a variable and assigning it the result of an addition operation
+total = 10 + 5
+#Printing the result
+print(total)
